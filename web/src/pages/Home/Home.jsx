8299fbab46b0b78e16a351406dbb0a897cf17d33
Dion Pinto <dionpinto000@gmail.com>
2021-02-25 19:39:58 +0530

    home forest mesh added

--- web/src/pages/Home/Home.jsx
+++ web/src/pages/Home/Home.jsx
@@ -2,9 +2,11 @@ import React, { Fragment, Suspense } from "react";
 
 import { Canvas } from "react-three-fiber";
 
-import { useGLTF, OrbitControls } from "@react-three/drei";
+import { useFBX, OrbitControls } from "@react-three/drei";
 
 import Navbar from "../../components/Navbar/Navbar";
+
+// const Forest =  require("../../../public/main_level.fbx")
 
 import "./Home.css";
 
@@ -12,11 +14,11 @@ const Lights = () => {
   return (
     <Fragment>
       <ambientLight intensity={0.3} />
-      <directionalLight position={[10, 10, 5]} intensity={1} />
+      <directionalLight position={[10, 10, 5]} intensity={0.7} />
       <directionalLight
         castShadow
         position={[0, 10, 0]}
-        intensity={1}
+        intensity={0.7}
         shadow-mapSize-width={1024}
         shadow-mapSize-height={1024}
         shadow-camera-far={50}
@@ -32,8 +34,8 @@ const Lights = () => {
 };
 
 function Model({ modelPath }) {
-  const gltf = useGLTF(modelPath);
-  return <primitive object={gltf.scene} dispose={null} />;
+  const fbx = useFBX(modelPath);
+  return <primitive object={fbx} dispose={null} />;
 }
 
 const Home = () => {
@@ -42,26 +44,25 @@ const Home = () => {
       <Navbar />
       <Canvas
         colorManagement
-        camera={{ position: [0, 0, 40], fov: 70 }}
+        camera={{ position: [0, 100, 0], fov: 70 }}
         style={{ height: "100vh" }}
       >
         <Lights />
         <Suspense fallback={null}>
           <group position={[0, 250, 0]}>
             <mesh
-              scale={[1, 1, 1]}
+              scale={[0.01, 0.01, 0.01]}
               onClick={(e) => {
                 e.stopPropagation();
                 console.log("yolo");
               }}
               position={[0, -250, 0]}
             >
-              <Model modelPath="/main_level.gltf" />
+              <Model modelPath="/main_level.fbx" />
             </mesh>
           </group>
         </Suspense>
         <OrbitControls
-          enableZoom={false}
           mixPolarAngle={Math.PI / 2.1}
           maxPolarAngle={Math.PI / 2.1}
         />
