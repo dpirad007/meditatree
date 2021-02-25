741612cf3d626695ee331b51fed7c9b80322b093
Dion Pinto <dionpinto000@gmail.com>
2021-02-25 20:03:10 +0530

    home forest lighting fix

--- web/src/pages/Home/Home.jsx
+++ web/src/pages/Home/Home.jsx
@@ -14,21 +14,10 @@ const Lights = () => {
   return (
     <Fragment>
       <ambientLight intensity={0.3} />
-      <directionalLight position={[10, 10, 5]} intensity={0.7} />
-      <directionalLight
-        castShadow
-        position={[0, 10, 0]}
-        intensity={0.7}
-        shadow-mapSize-width={1024}
-        shadow-mapSize-height={1024}
-        shadow-camera-far={50}
-        shadow-camera-left={-10}
-        shadow-camera-right={10}
-        shadow-camera-top={10}
-        shadow-camera-bottom={-10}
-      />
-      {/* Spotlight Large overhead light */}
-      <spotLight intensity={1} position={[1000, 0, 0]} castShadow />
+
+      <directionalLight position={[-10, 10, 0]} intensity={0.1} />
+      <directionalLight castShadow position={[0, 10, 0]} intensity={0.1} />
+      <directionalLight castShadow position={[100, 10, 0]} intensity={0.1} />
     </Fragment>
   );
 };
