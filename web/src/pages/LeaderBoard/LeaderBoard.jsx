e231d71401707664054bb207ab2174b0f5be3813
Dion Pinto <dionpinto000@gmail.com>
2021-02-25 20:03:57 +0530

    import removed

--- web/src/pages/LeaderBoard/LeaderBoard.jsx
+++ web/src/pages/LeaderBoard/LeaderBoard.jsx
@@ -4,8 +4,6 @@ import { Canvas } from "react-three-fiber";
 
 import { useGLTF, OrbitControls } from "@react-three/drei";
 
-import { Section } from "../../components/Section";
-
 import Navbar from "../../components/Navbar/Navbar";
 
 import "./LeaderBoard.css";
