26600b97d3b6f1676cc10b2991b3148a075aa08b
Dion Pinto <dionpinto000@gmail.com>
2021-02-26 10:22:37 +0530

    psuh

--- web/src/components/Navbar/Navbar.jsx
+++ web/src/components/Navbar/Navbar.jsx
@@ -1,38 +1,39 @@
-import React from 'react';
+import React from "react";
 
-import { Link, useLocation } from 'react-router-dom';
-import easyFetch from '../../utils/easyFetch';
+import { Link, useLocation } from "react-router-dom";
+import easyFetch from "../../utils/easyFetch";
 
-import './Navbar.css';
+import "./Navbar.css";
 
 export default function Header() {
   const location = useLocation();
 
   return (
     <header>
-      <div className='header-inner'>
-        <div className='logo'>
-          <a href='/'>MediTree</a>
+      <div className="header-inner">
+        <div className="logo">
+          <a href="/">MediTree</a>
         </div>
         <nav>
           <ul>
-            {['/', '/leaderboard', '/streaks']
-              .filter(p => p !== location.pathname)
-              .map(p => (
+            {["/", "/leaderboard", "/streaks"]
+              .filter((p) => p !== location.pathname)
+              .map((p) => (
                 <li key={p}>
-                  <Link to={p}>{p === '/' ? 'Home' : 'Leaderboard'}</Link>
+                  <Link to={p}>{p === "/" ? "Home" : "Leaderboard"}</Link>
                 </li>
               ))}
             <li>
               <button
                 style={{
-                  backgroundColor: 'transparent',
-                  border: 'none',
-                  color: 'white',
+                  backgroundColor: "transparent",
+                  border: "none",
+                  color: "white",
+                  cursor: "pointer",
                 }}
                 onClick={async () => {
-                  await easyFetch('auth/logout', {}, 'POST');
-                  window.location = '/login';
+                  await easyFetch("auth/logout", {}, "POST");
+                  window.location = "/login";
                 }}
               >
                 Log Out
