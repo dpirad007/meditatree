915876f9113ff96da0f647303cab6670952c96a9
Dion Pinto <dionpinto000@gmail.com>
2021-02-26 07:56:07 +0530

    lighting fix

--- web/src/pages/LeaderBoard/LeaderBoard.jsx
+++ web/src/pages/LeaderBoard/LeaderBoard.jsx
@@ -15,7 +15,7 @@ const Lights = () => {
   return (
     <Fragment>
       <ambientLight intensity={0.3} />
-      <directionalLight position={[10, 10, 5]} intensity={1} />
+      <directionalLight position={[10, 10, 5]} intensity={0.3} />
     </Fragment>
   );
 };
